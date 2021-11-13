ede8b12a82fc96a5d26aa64f0a248a59d01c7605
WillyHC22 <willy_chung@hotmail.com>
2021-11-13 20:42:00 +0800

    Skeleton code for using GPIO with our module

---
+++ control.py
@@ -0,0 +1,15 @@
+import time
+import RPi.GPIO as GPIO
+
+
+class MovementControl():
+    def __init__(self, servo):
+        self.servo = servo
+    def Anger(self):
+        pass
+    def Sadness(self):
+        pass
+    def Neutral(self):
+        pass
+    def Happy(self):
+        pass
