74689ae02a8624c9a0defc5a93ded3f3cd578293
WillyHC22 <willy_chung@hotmail.com>
2021-11-19 13:34:52 +0800

    Update parser argu

--- utils/argparser.py
+++ utils/argparser.py
@@ -9,6 +9,9 @@ def get_parser():
     parser.add_argument("-p3", "--servo_pin3", type=int, default=10, help="pin used on raspberry board for servo 3")
     parser.add_argument("-p4", "--servo_pin4", type=int, default=11, help="pin used on raspberry board for servo 4")
     parser.add_argument("--init_duty", type=int, default=0, help="Initial duty when all 4 servos start")
+    parser.add_argument("-d", "--depth", type=int, default=3, help="Depth of the trembling (3 -> Everytime it trembles, it goes 3 degrees back)")
+    parser.add_argument("-i", "--interval", type=int, default=5, help="Interval in terms of angle to make the servo tremble (5 -> Every 5 degrees, it trembles)")
+
 
     parser.add_argument("-sp", "--servo_pin", type=int, default=8, help="pin used on raspberry board")
     parser.add_argument("-ct", "--update_interval", default=3, help="Time interval between two updates for the emotion detection")
